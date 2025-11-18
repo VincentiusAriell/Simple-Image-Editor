915c8a4f1dcec44c9b7b2f5a237f201a19ac1432
VincentiusAriell <riellbelajarkomputer@gmail.com>
2025-11-18 22:22:03 +0700

    Delete ImageEdit directory

--- ImageEdit/main.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtWidgets
-from ui.window import MainWindow
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    win = MainWindow()
-    win.show()
-    app.exec_()
